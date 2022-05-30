a27bf4100004072dbc80ff552668297f09d1a6ad
Pl4sm434 <106500926+Pl4sm434@users.noreply.github.com>
2022-05-30 07:44:31 +0530

    Add files via upload

---
+++ Lava.java
@@ -0,0 +1,10 @@
+//Production Release 
+//Date: 5/23/22
+public class Lava extends Platform {
+	// Creates lava
+	public Lava(int sX, int sY, int sW, int sH) {
+		super(sX, sY, sW, sH);
+	}
+	
+	
+}
